b0bc714c2de7a7f8595d9dbf24d2e6954ca99686
Yagmur Karabiyik <yagmurbkarabiyik@gmail.com>
2024-03-04 17:23:17 +0300

    product init

--- ArtyfyBackend.Bll/Services/ProductService.cs
+++ ArtyfyBackend.Bll/Services/ProductService.cs
@@ -1,6 +1,8 @@
 ﻿using ArtyfyBackend.Core.Models.Category;
+using ArtyfyBackend.Core.Models.Common;
 using ArtyfyBackend.Core.Models.Product;
 using ArtyfyBackend.Core.Repositories;
+using ArtyfyBackend.Core.Responses;
 using ArtyfyBackend.Core.Services;
 using ArtyfyBackend.Core.UnitOfWork;
 using ArtyfyBackend.Domain.Entities;
@@ -12,6 +14,11 @@ namespace ArtyfyBackend.Bll.Services
     {
         public ProductService(IGenericRepository<Product> repository, IMapper mapper, IUnitOfWork unitOfWork) : base(repository, mapper, unitOfWork)
         {
+        }
+
+        public Task<Response<NoDataModel>> Create(ProductModel model)
+        {
+            throw new NotImplementedException();
         }
     }
 }
--- ArtyfyBackend.Core/IServices/IProductService.cs
+++ ArtyfyBackend.Core/IServices/IProductService.cs
@@ -1,9 +1,12 @@
-﻿using ArtyfyBackend.Core.Models.Product;
+﻿using ArtyfyBackend.Core.Models.Common;
+using ArtyfyBackend.Core.Models.Product;
+using ArtyfyBackend.Core.Responses;
 using ArtyfyBackend.Domain.Entities;
 
 namespace ArtyfyBackend.Core.Services
 {
     public interface IProductService : IGenericService<Product, ProductModel>
     {
+        Task<Response<NoDataModel>> Create(ProductModel model);
     }
 }
