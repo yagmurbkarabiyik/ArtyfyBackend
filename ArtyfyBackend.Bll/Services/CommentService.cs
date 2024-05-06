afc6a896c8da14f43d55e6da1efb20d62661e1dd
Yagmur Karabiyik <yagmurbkarabiyik@gmail.com>
2024-05-06 18:07:11 +0300

    unused using deleted

--- ArtyfyBackend.Bll/Services/CommentService.cs
+++ ArtyfyBackend.Bll/Services/CommentService.cs
@@ -1,24 +1,22 @@
 ﻿using ArtyfyBackend.Core.IServices;
 using ArtyfyBackend.Core.Models.Comment;
 using ArtyfyBackend.Core.Repositories;
-using ArtyfyBackend.Core.Responses;
 using ArtyfyBackend.Core.UnitOfWork;
 using ArtyfyBackend.Domain.Entities;
 using AutoMapper;
-using Microsoft.EntityFrameworkCore;
 
 namespace ArtyfyBackend.Bll.Services
 {
-    public class CommentService : GenericService<Comment, CommentModel>, ICommentService
-    {
-        private readonly ICommentRepository _commentRepository;
-        private readonly IMapper _mapper;
+	public class CommentService : GenericService<Comment, CommentModel>, ICommentService
+	{
+		private readonly ICommentRepository _commentRepository;
+		private readonly IMapper _mapper;
 
-        public CommentService(IUnitOfWork unitOfWork, IGenericRepository<Comment> repository, IMapper mapper,
-            ICommentRepository commentRepository) : base(repository, mapper, unitOfWork)
-        {
-            _commentRepository = commentRepository;
-        }
-    }
+		public CommentService(IUnitOfWork unitOfWork, IGenericRepository<Comment> repository, IMapper mapper,
+			ICommentRepository commentRepository) : base(repository, mapper, unitOfWork)
+		{
+			_commentRepository = commentRepository;
+		}
+	}
 }
 //todo createdDate problem
